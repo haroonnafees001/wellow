ed5bae6438daa9cf9b4a32829e9fe46229f15937
Haroon Nafees <haroon.nafees@gmail.com>
2024-11-13 17:38:49 +0500

    add calendly

---
+++ src/components/calendly/CalendlyPopup.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useState } from 'react';
+import { PopupModal } from 'react-calendly';
+
+type CalendlyPopupProps = {
+    url: string;
+};
+
+const CalendlyPopup: React.FC<CalendlyPopupProps> = ({ url }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpen = () => setIsOpen(true);
+    const handleClose = () => setIsOpen(false);
+
+    return (
+        <div>
+            <button onClick={handleOpen} className="box-content px-10 mt-5 text-sm  font-ProximaNova-Bold bg-color-purple text-white hover:opacity-85 hover:text-white rounded-[50px]  w-[185px] h-[51px]  transform transition ease-in-out duration-400   disabled:text-white disabled:cursor-not-allowed disabled:opacity-25 disabled:bg-slate-700 disabled:border-slate-700 disabled:hover:text-white">
+            Book an appointment
+            </button>
+            {isOpen && (
+                <PopupModal
+                    url={url}
+                    rootElement={document.getElementById('root') || document.body}
+                    onModalClose={handleClose}
+                    open={isOpen}
+                />
+            )}
+        </div>
+    );
+};
+
+export default CalendlyPopup;
--- src/components/card/BookAnAppointment.js
+++ src/components/card/BookAnAppointment.js
@@ -1,5 +1,7 @@
+"use client"
 import { web_assets } from "@/utils/constants";
 import Image from "next/image";
+import CalendlyPopup from "../calendly/CalendlyPopup";
 
 export default function BookAnAppointment() {
   return (
@@ -7,9 +9,9 @@ export default function BookAnAppointment() {
       <div className="border border-2 border-color-purple flex rounded-[30px] w-[827px] p-8 m-auto items-center Mobile:w-full Mobile:flex-col">
         <div className="w-7/12 Mobile:w-full">
             <b className="text-[24px]">Want a Customise use case for you?</b> <br/>
-            <button className="box-content px-10 mt-5 text-sm  font-ProximaNova-Bold bg-color-purple text-white hover:opacity-85 hover:text-white rounded-[50px]  w-[185px] h-[51px]  transform transition ease-in-out duration-400   disabled:text-white disabled:cursor-not-allowed disabled:opacity-25 disabled:bg-slate-700 disabled:border-slate-700 disabled:hover:text-white">
-            Book an appointment
-            </button>
+           
+            <CalendlyPopup url="https://calendly.com/shaheer-disrupt/wellows"/>
+            
         </div>
         <div className="w-1/3 flex justify-end Mobile:w-full Mobile:justify-center Mobile:mt-10">
         <Image
