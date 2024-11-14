ce43d87d760e208a323e26032b31658c419d71f2
Haroon Nafees <haroon.nafees@gmail.com>
2024-11-14 18:24:10 +0500

    fix issue in viralsweep

--- src/components/card/BookAnAppointment.js
+++ src/components/card/BookAnAppointment.js
@@ -15,7 +15,7 @@ export default function BookAnAppointment() {
 
           <CalendlyPopup url="https://calendly.com/shaheer-disrupt/wellows" />
         </div>
-        <div className="w-1/3 flex justify-end Mobile:w-full Mobile:justify-center Mobile:mt-10 Mobile:order-1 Mobile:mt-0">
+        <div className="w-1/3 flex justify-end Mobile:w-full Mobile:justify-center  Mobile:order-1 Mobile:mt-0 Mobile:mb-5">
           <Image
             src={web_assets + "book.gif"}
             width={100}
