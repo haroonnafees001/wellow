c8e762ca18a8805aa2071e735c693890c94b8582
Haroon Nafees <haroon.nafees@gmail.com>
2024-11-14 17:48:00 +0500

    ui work

--- src/components/card/BookAnAppointment.js
+++ src/components/card/BookAnAppointment.js
@@ -7,15 +7,15 @@ export default function BookAnAppointment() {
   return (
     <>
       <div className="border border-2 border-color-purple flex rounded-[30px] w-[827px] p-8 m-auto items-center Mobile:w-full Mobile:flex-col">
-        <div className="w-7/12 Mobile:w-full Mobile:text-center">
-          <p className="text-[24px]">
-            <b>Is your work unique?</b> <br />
+        <div className="w-7/12 Mobile:w-full Mobile:text-center Mobile:order-2">
+          <p>
+            <b className="text-[24px]">Is your work unique?</b> <br />
             Let’s discuss it, and our team will create a custom solution.
           </p>
 
           <CalendlyPopup url="https://calendly.com/shaheer-disrupt/wellows" />
         </div>
-        <div className="w-1/3 flex justify-end Mobile:w-full Mobile:justify-center Mobile:mt-10">
+        <div className="w-1/3 flex justify-end Mobile:w-full Mobile:justify-center Mobile:mt-10 Mobile:order-1 Mobile:mt-0">
           <Image
             src={web_assets + "book.gif"}
             width={100}
